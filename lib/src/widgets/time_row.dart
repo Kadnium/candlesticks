4a8e901262a9013975a64b076581e7ec5d66846b
Jani Niittymäki <jani.niitty@outlook.com>
2022-04-11 17:41:02 +0300

    Fix array out of range error

--- lib/src/widgets/time_row.dart
+++ lib/src/widgets/time_row.dart
@@ -97,6 +97,9 @@ class _TimeRowState extends State<TimeRow> {
   @override
   Widget build(BuildContext context) {
     int step = _stepCalculator();
+    if (step > widget.candles.length) {
+      step = widget.candles.length - 1;
+    }
     final dif = widget.candles[0].date.difference(widget.candles[step].date);
     return Padding(
       padding: const EdgeInsets.only(right: PRICE_BAR_WIDTH + 1.0),
